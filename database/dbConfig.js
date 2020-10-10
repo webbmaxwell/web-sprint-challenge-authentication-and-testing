871cea67625b236d5a11161b18fd1f45da6d39d5
webbmaxwell <webbwrites@gmail.com>
2020-10-09 21:10:38 -0600

    Tests written and passing

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,4 +3,4 @@ const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 const dbEnv = process.env.DB_ENV || 'development';
 
-module.exports = knex(knexConfig(dbEnv));
+module.exports = knex(knexConfig[dbEnv]);
